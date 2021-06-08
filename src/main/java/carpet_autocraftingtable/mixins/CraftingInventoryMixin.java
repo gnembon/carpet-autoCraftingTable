a6a224b071b69172d24e2617af2cd8e3904175fd
gnembon <gnembonmc@gmail.com>
2021-06-07 23:59:30 -0300

    1.4.39 and set to java 16

--- src/main/java/carpet_autocraftingtable/mixins/CraftingInventoryMixin.java
+++ src/main/java/carpet_autocraftingtable/mixins/CraftingInventoryMixin.java
@@ -4,11 +4,13 @@ import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(CraftingInventory.class)
 public interface CraftingInventoryMixin
 {
+    @Mutable
     @Accessor("stacks")
     void setInventory(DefaultedList<ItemStack> inventory);
 }
