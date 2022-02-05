f039c79b3582b18af41c28d01126368df28a770f
FX - PR0CESS <fx.e.morin@gmail.com>
2022-02-05 11:47:01 -0500

    These actually need to be in the right order

--- src/main/java/carpet_autocraftingtable/AutoCraftingTableContainer.java
+++ src/main/java/carpet_autocraftingtable/AutoCraftingTableContainer.java
@@ -27,8 +27,13 @@ public class AutoCraftingTableContainer extends AbstractRecipeScreenHandler<Craf
         this.addSlot(new OutputSlot(this.blockEntity, this.player));
 
         for(int y = 0; y < 3; ++y) {
+            for(int x = 0; x < 3; ++x) {
+                this.addSlot(new Slot(this.blockEntity, x + y * 3 + 1, 30 + x * 18, 17 + y * 18));
+            }
+        }
+
+        for(int y = 0; y < 3; ++y) {
             for(int x = 0; x < 9; ++x) {
-                if (x < 3) this.addSlot(new Slot(this.blockEntity, x + y * 3 + 1, 30 + x * 18, 17 + y * 18));
                 this.addSlot(new Slot(playerInventory, x + y * 9 + 9, 8 + x * 18, 84 + y * 18));
             }
         }
