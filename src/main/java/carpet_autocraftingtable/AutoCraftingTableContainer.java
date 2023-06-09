af5c647d9e4f2adff023db8216de032d6d773d55
gnembon <41132274+gnembon@users.noreply.github.com>
2023-06-09 22:24:35 +0200

    1.20

--- src/main/java/carpet_autocraftingtable/AutoCraftingTableContainer.java
+++ src/main/java/carpet_autocraftingtable/AutoCraftingTableContainer.java
@@ -116,7 +116,7 @@ public class AutoCraftingTableContainer extends AbstractRecipeScreenHandler<Craf
                 if(
                     player instanceof ServerPlayerEntity
                     && blockEntity.getLastRecipe() != null
-                    && !blockEntity.shouldCraftRecipe(player.world, (ServerPlayerEntity) player, blockEntity.getLastRecipe())
+                    && !blockEntity.shouldCraftRecipe(player.getWorld(), (ServerPlayerEntity) player, blockEntity.getLastRecipe())
                 ) {
                         return ItemStack.EMPTY;
                 }
@@ -179,8 +179,8 @@ public class AutoCraftingTableContainer extends AbstractRecipeScreenHandler<Craf
         @Override
         protected void onCrafted(ItemStack stack, int amount) {
             super.onCrafted(stack); // from CraftingResultsSlot onCrafted
-            if (amount > 0) stack.onCraft(this.player.world, this.player, amount);
-            if (this.inventory instanceof RecipeUnlocker) ((RecipeUnlocker)this.inventory).unlockLastRecipe(this.player);
+            if (amount > 0) stack.onCraft(this.player.getWorld(), this.player, amount);
+            if (this.inventory instanceof RecipeUnlocker) ((RecipeUnlocker)this.inventory).unlockLastRecipe(this.player, getStacks());
         }
 
         @Override
